694f7e397a425d3457e3f2f10ac86c85e313f645
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2018-09-02 21:05:19 +0100

    Added the node-mysql module. Also created the database connection.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var token = require("./config.js");
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -9,7 +9,8 @@ client.on('message', msg => {
   console.log('message'); // Show that things are going through for the time being.
   if (msg.content === 'ping') {
     msg.reply('Pong!');
+    console.log(token.selectQuery());
   }
 });
-token = require("./config.js");
+
 client.login(token.theToken());
