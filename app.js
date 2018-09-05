ead0770e24012f9903b7aa8f01e63769c8cced40
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2018-09-05 08:24:39 +0100

    commenting code

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+//required for .env file
 require('dotenv').config();
+
+//include discord.js
 const Discord = require('discord.js');
+
+//create new discord object
 const client = new Discord.Client();
 
+//when the discord bot connects...
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//when the bot recieves a message...
 client.on('message', msg => {
   console.log('message'); // Show that things are going through for the time being.
   if (msg.content === 'create') {
@@ -15,4 +22,5 @@ client.on('message', msg => {
   }
 });
 
+//connect the discord bot
 client.login(process.env.DISCORD_TOKEN);
--- functions/create_planner.js
+++ functions/create_planner.js
@@ -1,6 +1,9 @@
+//create a module.export that will be referenced in app.ja
 var createPlanner = module.exports = {
+  //create a function called "create Planner"
   createplanner: function(){
   var mysql = require('mysql');
+  //connect using mysql details of your localhost
   var connection = mysql.createConnection({
     host     : process.env.DBHOST,
     user     : process.env.DBUSERNAME,
@@ -9,13 +12,17 @@ var createPlanner = module.exports = {
     port : process.env.DBPORT
   });
   connection.connect();
+  //run your mysql query
     connection.query('select * from schedule ', function(err, rows, fields) {
+      //if it errors
       if (!err)
         console.log('The solution is: \n', rows);
+        //else success code
       else
         console.log('Error while performing Query.');
     });
     connection.end();
   }
 }
+//used to push information to app.js
 return module.exports;
