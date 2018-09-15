1ac377f94593c9b2cad99891d168602b604401f4
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2018-09-15 18:46:30 +0100

    Table setup ready to roll once basic db details are filled in.

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
 //required for .env file
 require('dotenv').config();
+//check for table
+
+///////////////////////
+// START TABLE SETUP
+///////////////////////
+
+var mysql = require('mysql');
+console.log("connect")
+var connection = mysql.createConnection({
+  host     : process.env.DBHOST,
+  user     : process.env.DBUSERNAME,
+  password : process.env.DBPASSWORD,
+  database : process.env.DBNAME,
+  port : process.env.DBPORT
+});
+connection.connect();
+console.log("connected");
+connection.query('CREATE TABLE IF NOT EXISTS schedule (schedule_id int(10) NOT NULL, admin_nickname varchar(254) NOT NULL, admin_discord_id bigint(18) NOT NULL, guild_id bigint(18) NOT NULL, PRIMARY KEY(schedule_id));', function(err, rows, fields) {
+   //CREATE TABLE  registered_users (id int(11) NOT NULL AUTO_INCREMENT, schedule_id int(10) NOT NULL, discord_schedule_user_id int(18) NOT NULL, PRIMARY KEY(id) FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id)); END
+  if (!err)
+    console.log('through 1');
+    //else success code
+  else
+    console.log('Error while creating table.' + err);
+});
+connection.query('CREATE TABLE IF NOT EXISTS registered_users (id int(11) NOT NULL AUTO_INCREMENT, schedule_id int(10) NOT NULL, discord_schedule_user_id int(18) NOT NULL, PRIMARY KEY(id), FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id));', function(err, rows, fields) {
+   //CREATE TABLE  CREATE TABLE  user_times (id int(11) NOT NULL AUTO_INCREMENT, schedule_id int(10) NOT NULL, discord_schedule_user_id int(18) NOT NULL, day int(11) NOT NULL, hour int(11) NOT NULL, PRIMARY KEY(id) FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id)); END
+  if (!err)
+    console.log('through 2');
+    //else success code
+  else
+    console.log('Error while creating table.' + err);
+});
+connection.query('CREATE TABLE IF NOT EXISTS user_times (id int(11) NOT NULL AUTO_INCREMENT, schedule_id int(10) NOT NULL, discord_schedule_user_id int(18) NOT NULL, day int(11) NOT NULL, hour int(11) NOT NULL, PRIMARY KEY(id), FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id));', function(err, rows, fields) {
+   //CREATE TABLE  CREATE TABLE  user_times (id int(11) NOT NULL AUTO_INCREMENT, schedule_id int(10) NOT NULL, discord_schedule_user_id int(18) NOT NULL, day int(11) NOT NULL, hour int(11) NOT NULL, PRIMARY KEY(id) FOREIGN KEY (schedule_id) REFERENCES schedule(schedule_id)); END
+  if (!err)
+    console.log('through 3');
+    //else success code
+  else
+    console.log('Error while creating table.' + err);
+});
+connection.end();
+
+console.log("connection end");
+
+///////////////////////
+// END TABLE SETUP
+///////////////////////
 
 //include discord.js
 const Discord = require('discord.js');
@@ -14,7 +62,6 @@ client.on('ready', () => {
 
 //when the bot recieves a message...
 client.on('message', msg => {
-  console.log('message'); // Show that things are going through for the time being.
   if (msg.content === 'create') {
     const createPlanner = require('./functions/create_planner.js');
     var results = createPlanner.createplanner();
