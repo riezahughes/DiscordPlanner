6a57b047522612c84e479c43bb4e1ac87cbdb52a
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2018-12-06 19:34:57 +0000

    update to include posting messages.

--- app.js
+++ app.js
@@ -84,6 +84,55 @@ client.on('message', msg => {
     let results = displayPlanner.displayPlanner(msg.author.id);
     console.log(msg.author.id);
   }
+  if (msg.content === '!upload') {
+    //msg.channel.send("Test", { file: "https://puu.sh/C711d/29d8f2f79e.png" });
+    const embed = new Discord.RichEmbed()
+      .setTitle("This is your title, it can hold 256 characters")
+      .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+      /*
+       * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+       */
+      .setColor(0x00AE86)
+      .setDescription("This is the main body of text, it can hold 2048 characters.")
+      .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+      .setImage("http://i.imgur.com/yVpymuV.png")
+      .setThumbnail("http://i.imgur.com/p2qNFag.png")
+      /*
+       * Takes a Date object, defaults to current date.
+       */
+      .setTimestamp()
+      .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+      .addField("This is a field title, it can hold 256 characters",
+        "This is a field value, it can hold 1024 characters.")
+      /*
+       * Inline fields may not display as inline if the thumbnail and/or image is too big.
+       */
+      .addField("Inline Field", "They can also be inline.", true)
+      /*
+       * Blank field, useful to create some space.
+       */
+      .addBlankField(true)
+      .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+    msg.channel.send(embed);
+    console.log("Created Message");
+  }
+  if (msg.content === '!edit'){
+    console.log("trying");
+
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setImage("http://puu.sh/Cb9Pd.png")
+
+    let getid = msg.channel.fetchMessage("518415883371020299")
+    .then(message => message.edit(embed));
+
+    /*
+    .then( message => message.edit("Again, NOPE", { file: "http://puu.sh/CaEJa.jpg"}))
+    .then( console.log("finished edit"))
+    .catch( error => console.log("nyet"));
+    */
+  }
 });
 
 //connect the discord bot
